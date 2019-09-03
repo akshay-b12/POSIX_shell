5b34b4e3e2d966f3a51a1fc05b3edbbf07478e58
akshay-b12 <akshay.bankar@students.iiit.ac.in>
2019-09-03 15:47:28 +0530

    Initial source and header with shell functionalities : able to run commands with multiple args, support for $

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cstring>
+#include <unistd.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+#endif
